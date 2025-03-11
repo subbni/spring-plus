b6a01cc814c63735188e76efdab3135bdd544034
subbni <slfkak@naver.com>
2025-03-11 15:20:15 +0900

    feat(Lv.1): 3. todo 검색 조회 구현

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,16 +3,27 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    @EntityGraph(attributePaths = {"user"})
+    @Query("SELECT t FROM Todo t WHERE " +
+            "(:weather IS NULL OR t.weather = :weather) AND " +
+            "(:startDate IS NULL OR t.modifiedAt >= :startDate) AND " +
+            "(:endDate IS NULL OR t.modifiedAt <= :endDate) ")
+    Page<Todo> findAllBySearchConditions(
+            String weather,
+            LocalDateTime startDate,
+            LocalDateTime endDate,
+            Pageable pageable
+    );
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
