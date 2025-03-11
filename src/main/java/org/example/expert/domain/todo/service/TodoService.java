b67d2e9aca25eb77b4371199512f295498d023d5
subbni <slfkak@naver.com>
2025-03-11 15:20:15 +0900

    feat(Lv.1): 3. todo 검색 조회 구현

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -14,8 +14,13 @@ import org.example.expert.domain.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Service
 @RequiredArgsConstructor
@@ -48,10 +53,23 @@ public class TodoService {
         );
     }
 
-    public Page<TodoResponse> getTodos(int page, int size) {
-        Pageable pageable = PageRequest.of(page - 1, size);
+    public Page<TodoResponse> getTodos(
+            String weather,
+            LocalDate startDate,
+            LocalDate endDate,
+            int page,
+            int size
+    ) {
+        Pageable pageable = PageRequest.of(page - 1, size, Sort.Direction.DESC, "modifiedAt");
+        LocalDateTime startDateTime = (startDate != null) ? startDate.atStartOfDay() : null;
+        LocalDateTime endDateTime = (endDate != null) ? endDate.atTime(23, 59) : null;
 
-        Page<Todo> todos = todoRepository.findAllByOrderByModifiedAtDesc(pageable);
+        Page<Todo> todos = todoRepository.findAllBySearchConditions(
+                weather,
+                startDateTime,
+                endDateTime,
+                pageable
+        );
 
         return todos.map(todo -> new TodoResponse(
                 todo.getId(),
