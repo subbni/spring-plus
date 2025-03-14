ea6f519e121f7cd5a3bd03fb38f97a0f4e560821
subbni <slfkak@naver.com>
2025-03-14 20:10:20 +0900

    feat(Lv.3): 12-1. health check api 작성

---
+++ src/main/java/org/example/expert/domain/monitoring/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package org.example.expert.domain.monitoring.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+}
